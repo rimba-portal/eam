9ea59cab5c2dd5a441043e7900e8cf8627b5db41
Faros <114036707+a-faros@users.noreply.github.com>
2026-08-05 12:47:35 +0800

    Release v1.0.5

--- src/EamServiceProvider.php
+++ src/EamServiceProvider.php
@@ -6,16 +6,18 @@ namespace Rimba\Eam;
 
 use Rimba\Base\Services\BitesServiceProvider;
 
+
 class EamServiceProvider extends BitesServiceProvider
 {
+
     protected function bootPackage(): void
     {
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
         //
     }
-
     protected function registerPackage(): void
     {
         //
     }
+
 }
